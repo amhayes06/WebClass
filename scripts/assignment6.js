f0c8e98d8023ad905a18c8f0c1d1436134154d37
Andrew <amhayes06@gmail.com>
2021-09-16 11:12:44 -0600

    as6

--- scripts/assignment6.js
+++ scripts/assignment6.js
@@ -16,5 +16,5 @@ function populateTable(jsonObj) {
         text += "<tr><td>" + jsonObj[x].firstName + "</td></tr>"
     }
     text += "</table>"
-    document.getElementById("table").innerHTML = jsonOb[1].firstName;
+    document.getElementById("table").innerHTML = jsonObj[1].firstName;
 }
