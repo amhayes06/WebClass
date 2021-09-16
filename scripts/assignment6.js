ef61205977e15ffe1db87c7685e32725ac1e46b6
Andrew <amhayes06@gmail.com>
2021-09-16 14:07:33 -0600

    a6 data

--- scripts/assignment6.js
+++ scripts/assignment6.js
@@ -11,7 +11,8 @@ function populateTable(jsonObj) {
     let text = "<table><th>Phase</th><th>"
     for (let x in jsonObj) {
         text += "<tr><td>" + jsonObj[x].phase + "</td>"
-        text += "<td><img src=\"" + jsonObj[x].image + "\"></td></tr>"
+        text += "<td><img src=\"" + jsonObj[x].image + "\"></td>"
+        text += "<td>" + jsonObj[x].details + "</td></tr>"
     }
     text += "</table>"
     document.getElementById("table").innerHTML = text;
