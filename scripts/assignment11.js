38509926406889c83e74bfb11cd6d0c7f45e447e
Andrew <amhayes06@gmail.com>
2021-11-08 10:05:29 -0700

    Start assignment11

---
+++ scripts/assignment11.js
@@ -0,0 +1,22 @@
+let requestURL = "./static/assignment11.json";
+let request = new XMLHttpRequest();
+const dbParam = JSON.stringify({table:"courses"})
+
+request.onload = function() {
+    const data = JSON.parse(this.responseText);
+    populateTable(data);
+}
+
+function populateTable(jsonObj) {
+    let text = "<table><th>Phase</th><th>Image</th><th>Details</th>"
+    for (let x in jsonObj) {
+        text += "<tr><td>" + jsonObj[x].phase + "</td>"
+        text += "<td><img src=\"" + jsonObj[x].image + "\"></td>"
+        text += "<td>" + jsonObj[x].details + "</td></tr>"
+    }
+    text += "</table>"
+    document.getElementById("table").innerHTML = text;
+}
+
+request.open('GET', requestURL);
+request.send("x=" + dbParam);
