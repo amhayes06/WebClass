a220bc69f3ffd84f115e80532323e7b29fb2e431
Andrew <amhayes06@gmail.com>
2021-12-07 09:41:40 -0700

    Update Game

--- scripts/assignment14.js
+++ scripts/assignment14.js
@@ -126,6 +126,18 @@ function collisionDetection() {
   }
 }
 
+function drawScore() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
+}
+
+function drawLives() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "0095DD";
+  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBricks();
